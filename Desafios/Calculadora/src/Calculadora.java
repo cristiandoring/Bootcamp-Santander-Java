01e44c490f72d97dd4be1b6fa3f366c971db2071
cristiandoring <cristiandoring@gmail.com>
2024-05-07 20:23:00 -0300

    Create Calculadora

---
+++ Desafios/Calculadora/src/Calculadora.java
@@ -0,0 +1,7 @@
+public class Calculadora {
+    
+    public int somar(int numero1, int numero2){
+    return numero1 + numero2;
+        
+    }
+}
---
+++ Desafios/Calculadora/src/Saida.java
@@ -0,0 +1,8 @@
+public class Saida {
+        
+    
+    public static void main(String[] args) {
+        int soma = somar(1,2);
+        System.out.println(soma);
+    }
+}
