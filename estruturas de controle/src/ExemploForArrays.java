1679d38772587cc91748baa88a7105cddfcb9b9a
cristiandoring <cristiandoring@gmail.com>
2024-05-10 15:34:42 -0300

    Create ExemploFor.java

---
+++ estruturas de controle/src/ExemploForArrays.java
@@ -0,0 +1,10 @@
+public class ExemploForArrays { 
+    public static void main(String[] args) {
+        String alunos[] = {"FELIPE", "CAIQUE", "JÚLIA", "MARIA"};
+
+        for (int i=0;i<alunos.length();i++){
+            System.out.println();
+        }
+    }
+    
+}
