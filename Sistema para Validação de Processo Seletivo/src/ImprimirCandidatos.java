4ce9570fce11919e6c0e8e81020ab7125bce93b2
cristiandoring <cristiandoring@gmail.com>
2024-05-16 12:16:04 -0300

    Create ImprimirCandidatos.java

---
+++ Sistema para Validação de Processo Seletivo/src/ImprimirCandidatos.java
@@ -0,0 +1,46 @@
+import java.util.concurrent.ThreadLocalRandom;
+
+public class ImprimirCandidatos{
+    public static void main(String[] args) throws Exception {
+        imprimirSelecionados();
+    }
+
+    static void imprimirSelecionados(){
+        String [] candidatos = {"FELIPE", "MARCIA", "JULIA", "PAULO", "AUGUSTO"};
+
+        System.out.println("Imprimindo os candidatos selecionados");
+
+        for(int indice=0;indice < candidatos.length; indice++){
+            System.out.println("O candidato de nº " + (indice+1) + " é " + candidatos[indice]);
+        }
+    }
+    static void selecaoCandidatos(){
+        String [] candidatos = {"FELIPE", "MARCIA", "JULIA", "PAULO", "AUGUSTO", "MONICA", "FABRÍCIO", "MIRELA", "DANIELA", "JORDANA" };
+
+        int candidatosSelecionados = 0;
+        int candidatoAtual = 0;
+        double salarioBase = 2000.0;
+
+        while (candidatosSelecionados<5 && candidatoAtual < candidatos.length){
+            String candidato = candidatos[candidatoAtual];
+            double salarioPretendido = valorPretendido();
+
+            System.out.println("O candidato " + candidato + " solicitou este valor de salário " + salarioPretendido);
+
+            if(salarioBase >= salarioPretendido){
+                System.out.println("O candidato " + candidato + " foi selecionado para a vaga");
+                candidatosSelecionados++;
+
+            }
+
+            candidatoAtual++;
+        }
+    
+    }
+
+    //retorna um valor aleatório entre 1800 e 2200 atribuindo ao método valorPretendido
+    static double valorPretendido(){
+        return ThreadLocalRandom.current().nextDouble(1800,2200);
+    }
+
+}
