6ba4dcefea7ebe2cc194e709476b8e113df71bcf
cristiandoring <cristiandoring@gmail.com>
2024-05-16 12:04:33 -0300

    Create SelecionandoCandidatos.java

---
+++ Sistema para Validação de Processo Seletivo/src/SelecionandoCandidatos.java
@@ -0,0 +1,51 @@
+import java.util.concurrent.ThreadLocalRandom;
+
+public class SelecionandoCandidatos{
+    public static void main(String[] args) throws Exception {
+        selecaoCandidatos();
+    }
+
+    static void selecaoCandidatos(){
+        String [] candidatos = {"FELIPE", "MARCIA", "JULIA", "PAULO", "AUGUSTO", "MONICA", "FABRÍCIO", "MIRELA", "DANIELA", "JORDANA" };
+
+        int candidatosSelecionados = 0;
+        int candidatoAtual = 0;
+        double salarioBase = 2000.0;
+
+        while (candidatosSelecionados<5) {
+            String candidato = candidatos[candidatoAtual];
+            double salarioPretendido = valorPretendido();
+
+            System.out.println("O candidato " + candidato + " solicitou este valor de salário " + salarioPretendido);
+
+            if(salarioBase >= salarioPretendido){
+                System.out.println("O candidato " + candidato + " foi selecionado para a vaga");
+                candidatosSelecionados++;
+
+            }
+
+            candidatosSelecionados++;
+        }
+    
+    }
+
+    //retorna um valor aleatório entre 1800 e 2200 atribuindo ao método valorPretendido
+    static double valorPretendido(){
+        return ThreadLocalRandom.current().nextDouble(1800,2200);
+    }
+
+
+    static void analisarCandidato(double salarioPretendido){
+        double salarioBase = 2000.0;
+
+        if (salarioBase > salarioPretendido){
+            System.out.println("Ligar para candidato.");
+        }
+        else if (salarioBase == salarioPretendido){
+            System.out.println("Ligar para o candidato com contra proposta");
+        }
+        else{
+            System.out.println("Aguardando o resultado dos demais candidatos");
+        }
+    }
+}
