62d55c3dc293dd2803e412c583daf12ada7d43a8
cristiandoring <cristiandoring@gmail.com>
2024-05-27 11:55:15 -0300

    Create Cliente.java

---
+++ ExemplosOrientacaoObjetos/src/lanchonete/Cliente.java
@@ -0,0 +1,13 @@
+package lanchonete;
+
+import java.security.PublicKey;
+
+public class Cliente {
+    public void escolherLanche(){
+        System.out.println("ESCOLHENDO LANCHE");
+    }
+
+    public void fazerPedido(){
+        System.out.println("FAZENDO PEDIDO");
+    }
+}
