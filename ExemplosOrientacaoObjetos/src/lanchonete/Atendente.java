78ce946d16d66b5e9b273551e25087caf94735c5
cristiandoring <cristiandoring@gmail.com>
2024-05-27 12:04:12 -0300

    Create Atendente.java

---
+++ ExemplosOrientacaoObjetos/src/lanchonete/Atendente.java
@@ -0,0 +1,15 @@
+package lanchonete;
+
+public class Atendente {
+    public void servindoMesa(){
+        System.out.println("SERVINDO MESA");
+    }
+
+    public void pegarLancheCozinha(){
+        System.out.println("PEGANDO LANCHE NA COZINHA");
+    }
+
+    public void receberPagamento(){
+        System.out.println("RECEBENDO PAGAMENTO");
+    }
+}
