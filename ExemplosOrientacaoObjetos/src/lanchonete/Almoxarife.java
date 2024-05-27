20701e78b908cdd0f8beb8c45d655ec24608bc00
cristiandoring <cristiandoring@gmail.com>
2024-05-27 12:01:52 -0300

    Create Almoxarife.java

---
+++ ExemplosOrientacaoObjetos/src/lanchonete/Almoxarife.java
@@ -0,0 +1,16 @@
+package lanchonete;
+
+public class Almoxarife {
+    public void controlarEntrada(){
+        System.out.println("CONTROLANDO ENTRADA");
+    }
+
+    public void controlarSaida(){
+        System.out.println("CONTROLANDO SAÍDA");
+    }
+
+    public void entregarIngredientes(){
+        System.out.println("ENTREGANDO INGREDIENTES");
+    }
+
+}
