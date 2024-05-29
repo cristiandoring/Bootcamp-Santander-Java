37bf0456cc3fed0f74890201b6cd91cb57d8d9dd
cristiandoring <cristiandoring@gmail.com>
2024-05-29 16:04:30 -0300

    Criei pacote MSN

---
+++ ExemplosOrientacaoObjetos/src/MSN/MSNMessenger.java
@@ -0,0 +1,19 @@
+package MSN;
+
+public class MSNMessenger {
+    public void enviarMensagem(){
+        System.out.println("Enviando Mensagem");
+    }
+
+    public void receberMensagem(){
+        System.out.println("Recebendo Mensagem");
+    }
+
+    public void validarConectadoInternet(){
+        System.out.println("Validando se está conectado a internet");
+    }
+
+    public void salvarHistoricoMensagem(){
+        System.out.println("Salvando o histórico da mensagem");
+    }
+}
