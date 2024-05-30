9225028eb1d5f960c31c68eb64d172f267155cf7
cristiandoring <cristiandoring@gmail.com>
2024-05-30 18:26:38 -0300

    Create pacote servicoMensagemInstantanea

---
+++ ExemplosOrientacaoObjetos/src/serviçoMensagemInstantanea/ServicoMensagemInstantanea.java
@@ -0,0 +1,23 @@
+package serviçoMensagemInstantanea;
+
+public class ServicoMensagemInstantanea {
+    public class MSNMessenger {
+        public void enviarMensagem(){
+            System.out.println("Enviando Mensagem");
+        }
+    
+        public void receberMensagem(){
+            System.out.println("Recebendo Mensagem");
+        }
+    
+        //tornando os métodos privados para que os computadores dos usuarios não vejam eles
+        //logo, para eles aparecerá apenas as opções de enviar e receber mensagens
+        
+        private void validarConectadoInternet(){
+            System.out.println("Validando se está conectado a internet");
+        }
+    
+        private void salvarHistoricoMensagem(){
+            System.out.println("Salvando o histórico da mensagem");
+        }
+}
