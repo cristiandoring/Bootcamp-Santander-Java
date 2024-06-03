64f449ef7ee891a483e3b2864cc2abc99af600d4
cristiandoring <cristiandoring@gmail.com>
2024-06-03 11:11:08 -0300

    Update Box.java

--- Java Colletions/src/Box.java
+++ Java Colletions/src/Box.java
@@ -22,13 +22,4 @@ public class Box <T> {
     public void set(T t){
         this.t = t;
     }
-
-    public T get(){
-        return t;
-    }
-
-    public Object get(){
-        return object;
-    }
-
 }
