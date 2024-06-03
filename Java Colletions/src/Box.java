7ca20428a6d181f78a1aff8f5fcaddf6c8a17f10
cristiandoring <cristiandoring@gmail.com>
2024-06-03 11:10:22 -0300

    Criei a classe genérica Box <T>

---
+++ Java Colletions/src/Box.java
@@ -0,0 +1,34 @@
+/* classe normal 
+public class Box {
+    //<T> representa "Type" 
+    private Object object;
+
+    public void set(Object object){
+        this.object = object;
+    }
+
+    public Object get(){
+        return object;
+    }
+
+}
+*/
+
+//classe do tipo genérica
+public class Box <T> {
+    //<T> representa "Type" 
+    private T t;
+
+    public void set(T t){
+        this.t = t;
+    }
+
+    public T get(){
+        return t;
+    }
+
+    public Object get(){
+        return object;
+    }
+
+}
