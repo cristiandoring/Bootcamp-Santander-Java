ce57c3204953a61466483532d8faf3801efb5e0f
cristiandoring <cristiandoring@gmail.com>
2024-06-10 12:54:48 -0300

    Update Example.Predicate

--- Stream API/src/PredicateExample.java
+++ Stream API/src/PredicateExample.java
@@ -4,7 +4,7 @@ import java.util.function.Predicate;
 
 public class PredicateExample {
     public static void main(String[] args) {
-        List<String> palavras = Arrays.asList("java", "python", "javascript");
+        List<String> palavras = Arrays.asList("java", "python", "javascript", "kotlin", "c");
 
         Predicate<String> maisdeCincoCaracteres = palavra -> palavra.length()>5;
 
