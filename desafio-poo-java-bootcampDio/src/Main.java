7981b0de8171351d115df8cf10f28ecd4065e84d
cristiandoring <cristiandoring@gmail.com>
2024-06-14 14:54:46 -0300

    Correção dos pacotes

--- desafio-poo-java-bootcampDio/src/Main.java
+++ desafio-poo-java-bootcampDio/src/Main.java
@@ -1,26 +1,28 @@
+import java.time.LocalDate;
 import br.com.dio.desafio.dominio.Curso;   
+import br.com.dio.desafio.dominio.Mentoria;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Curso curso1 = new Curso();
-        
-
         curso1.setTitulo("Curso Java");
         curso1.setDescricao("Curso de POO em Java");
-        curso1.cargaHoraria(8);
-        
-        System.out.println(curso1);
+        curso1.setCargaHoraria(8);  // Correção aqui
 
+        System.out.println(curso1);
 
         Curso curso2 = new Curso();
         curso2.setTitulo("Curso Python");
         curso2.setDescricao("Curso de POO em Python");
-        curso2.cargaHoraria(12);
-
+        curso2.setCargaHoraria(12);  // Correção aqui
 
         System.out.println(curso2);
 
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria de Java");
+        mentoria.setDescricao("Curso mentorado por ex alunos do projeto");  // Correção aqui
+        mentoria.setData(LocalDate.now());
 
-
+        System.out.println(mentoria);
     }
 }
